f41156e102db238cfbe6228914aa9053d335ba9d
CN-Aryz <aryz0713@gmail.com>
2023-03-16 00:25:19 +0800

    init commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('prettier').Options}
+ */
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  arrowParens: "avoid",
+  endOfLine: "auto",
+};
